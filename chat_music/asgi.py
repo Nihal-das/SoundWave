aef49448e8be8fb6a7add0cf741acf2b03a18618
NIHAL DAS <nihaldas8888@gmail.com>
2025-06-06 16:41:09 +0530

    new asgi changes

--- chat_music/asgi.py
+++ chat_music/asgi.py
@@ -1,16 +1,13 @@
 import os
 import django
-from channels.routing import ProtocolTypeRouter, URLRouter
-from django.core.asgi import get_asgi_application
-import chat.routing
-from channels.auth import AuthMiddlewareStack
-
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'chat_music.settings')
+django.setup()  # <-- Always call this before importing anything Django-dependent
 
-
-django.setup()
-
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.auth import AuthMiddlewareStack
+from django.core.asgi import get_asgi_application
+import chat.routing
 
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
